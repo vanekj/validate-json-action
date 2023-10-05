c1582957587b93b10ad75e705874dc8c396170cf
viren.mistry@quant.network <viren.mistry@quant.network>
2023-10-05 16:26:32 +0100

    Feat: Add in an option to use the ajv-formats plugin
    Hi vanekj
    We have been working on a Workflow that needed an AJV validator to help validate our Yaml files. When searching for AJV actions which could do the job, we stumbled upon yours, which we liked. This action was easy to understand, and we thought it could be the base of what we needed.
    
    With these Actions, we have added a few needed features to make it work within our project and could benefit others looking for the same thing. We have added in:
    
    An option to use ajv-format plugin. Great for when you use formats not supported by AJV alone.
    An option to allow matching properties. We needed this for our projects because we used "properties" and "patternProperties".
    An option to show validation results in a workflow summary. This is for quick analysis when you want to avoid looking at logs.
    We hope you like the changes we have made.

--- README.md
+++ README.md
@@ -16,9 +16,28 @@ no schema with key or ref "https://json-schema.org/draft-07/schema"
 
 **Required** Relative path to the JSON schema file
 
+### `additional_schema`
+
+**Required if schema use $ref to target other schemas.** Path to additional JSON schema files which is referenced by the schema used for validation. You can target one file or a directory with multiple files. (eg. schemas/ or schemas.schema.json).
+
+### `additional_schema_extention`
+
+**Required for additional_schema which are not .schema.json files.** The file extention of the schema used. ( default: .schema.json )
+
 ### `pattern`
 
 **Required** Glob pattern to the JSON(s) to validate
+
+### `allow_matching_properties`
+
+**Optional** When true, allow overlap between "properties" and "patternProperties". Does not affect other strict mode restrictions. (Defaults: "false")
+
+### `use_ajv_formats`
+
+**Optional** When true, allows you to use ajv-format plugin. Please follow the link to see what formats are allowed: https://ajv.js.org/packages/ajv-formats.html (Defaults: "false")
+
+### `show_result_in_summary`
+**Optional** When true, allow the results and error (if any) appear in the summary as a code block. (Defaults: "false")
 
 ## Outputs
 
@@ -26,9 +45,61 @@ List of all processed files and it's validation result.
 
 ## Example usage
 
+### Simple Validation
+Above is able to validate JSON and YAML files using a JSON schema. All you need to do to validate is to provide the path of where to find the Scheme using the `schema` key and the file pattern to validate under the `pattern` key. 
 ```yml
 uses: vanekj/validate-json-action@v1
 with:
   schema: schemas/default.schema.json
   pattern: data/**/*.json
 ```
+
+### Validation with definition schemas
+Above is able to validate JSON and YAML files using multiple JSON schema. To validate with multiple schemas, provide the path of where to find the Scheme using the `schema` key, add in the definition schemas using the `additional_schemas` key word, (if not using `schema.json` extention) the `additional_schema_extention` key and the file pattern to validate under the `pattern` key.
+```yml
+uses: vanekj/validate-json-action@v1
+with:
+  schema: schemas/default.schema.yaml
+  additional_schemas: schemas/
+  additional_schema_extention: schema.yaml
+  pattern: data/**/*.json
+```
+
+### Validate against schemas that use string formats: `use_ajv_formats`
+ If you are using formats in your schema (https://json-schema.org/understanding-json-schema/reference/string.html#format) set `use_ajv_formats` true and the plugin will be used. Example below:
+
+ ```yaml
+ uses: vanekj/validate-json-action@v1
+ with:
+   schema: schemas/default.schema.json
+   pattern: data/**/*.json
+   use_ajv_formats: "true"
+ ```
+
+### Validate against schemas with overlapping "properties" and "patternProperties": `allow_matching_properties`
+ If you see an error about a schema containing overlap between "properties" and "patternProperties", you can enable `allowmatchingProperties` behaviour with the `allow_matching_propertes` option 
+Example below:
+ ```yaml
+ uses: vanekj/validate-json-action@v1
+ with:
+   schema: schemas/default.schema.json
+   pattern: data/**/*.json
+   allow_matching_properties: "true"
+ ```
+
+### Show Validation results in github actions summary
+You have an option to display the results of the validator in a github summary. When making `show_result_in_summary` true, the results found in workflow logs will display within a codeblock in your github summary. Example below:
+#### Step in workflow
+```yaml
+uses: vanekj/validate-json-action@v1
+with:
+  schema: schemas/default.schema.json
+  pattern: data/**/*.json
+  show_result_in_summary: "true"
+``` 
+#### Result in Summary
+##### Pass Validation
+![Alt text](READMESnapshots/PassedResultSummary.png)
+
+##### Falied Validation
+![Alt text](READMESnapshots/FailedResutlsSummary.png)
--- src/action.js
+++ src/action.js
@@ -1,23 +1,28 @@
 // Load node modules
-import fs from 'node:fs/promises';
-import path from 'node:path';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import path from 'path';
 
 // Load additional modules
 import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
 import core from '@actions/core';
 import glob from '@actions/glob';
 import styles from 'ansi-styles';
+import YAML from 'yaml';
 
 // Load action inputs variables
 const schemaInput = core.getInput('schema');
 const patternInput = core.getInput('pattern');
+const additionalSchemaInput = core.getInput('additional_schema');
+const schemaExtentionInput = core.getInput('additional_schema_extention');
 
 // Load path to the checked out repository
 const repositoryPath = process.env.GITHUB_WORKSPACE;
 
 // Load the provided schema
 const schemaPath = path.join(repositoryPath, schemaInput);
-const schemaString = await fs.readFile(schemaPath, 'utf-8');
+const schemaString = await fsPromises.readFile(schemaPath, 'utf-8');
 const schemaObject = JSON.parse(schemaString);
 
 // Get all jsons paths
@@ -29,19 +34,66 @@ const globber = await glob.create(patternInput, {
 });
 
 // Init Ajv schema validator
-const validator = new Ajv({ allErrors: true });
-const validateFile = validator.compile(schemaObject);
+const validatorInput = { allErrors: true }
+
+if (core.getBooleanInput('allow_matching_properties')) {
+	validatorInput.allowMatchingProperties = true;
+}
+
+const validator = new Ajv(validatorInput);
+
+// Add formats if wanted
+if (core.getBooleanInput('use_ajv_formats')) {
+	core.debug('using ajv-formats with json-validator')
+	addFormats(validator)
+} else {
+	core.debug('ajv-formats will not be used with the json-validator')
+}
+
+//Add in additional schemas
+const additionalSchemaPath = path.join(repositoryPath, additionalSchemaInput);
+var additionalSchemaObjectArray = [];
+let fileDetails = await fs.lstatSync(additionalSchemaPath);
+var validateFile
+if(additionalSchemaInput.length === 0){
+	validateFile = validator.compile(schemaObject);
+}
+else if(fileDetails.isFile()){
+	additionalSchemaObjectArray.push(await extractFile(additionalSchemaPath));
+	validateFile = validator.addSchema(additionalSchemaObjectArray).compile(schemaObject);
+}
+else if(fileDetails.isDirectory()){
+	const files = await fsPromises.readdir(additionalSchemaPath);
+	for(var i = 0; i < files.length; i++){
+		var additionalSchemaFilePath = path.join(additionalSchemaPath, files[i]);
+		let fileDetails = await fs.lstatSync(additionalSchemaFilePath);
+		if(additionalSchemaFilePath != schemaPath && fileDetails.isFile() && additionalSchemaFilePath.includes(schemaExtentionInput)){
+			additionalSchemaObjectArray.push(await extractFile(additionalSchemaFilePath));
+		}
+	}
+	validateFile = validator.addSchema(additionalSchemaObjectArray).compile(schemaObject);
+}
+
+
+async function extractFile(filePath){
+	const fileContents = await fsPromises.readFile(filePath, 'utf-8');
+  const convertedToObject = JSON.parse(fileContents);
+	return convertedToObject;
+}
 
 // Define errors counter so we can return correct exit code
 let errorsCounter = 0;
+let testCounter = 0;
+
+var errorOutput = ``;
 
 // Validate each file according to schema
 for await (const jsonPath of globber.globGenerator()) {
 	// Load JSON file as a string
-	const jsonString = await fs.readFile(jsonPath, 'utf-8');
+	const jsonString = await fsPromises.readFile(jsonPath, 'utf-8');
 
 	// Parse the JSON string to an Object so the validator could handle it
-	const jsonObject = JSON.parse(jsonString);
+	const jsonObject = YAML.parse(jsonString);
 
 	// Validate the file according to given schema
 	const validationResult = validateFile(jsonObject);
@@ -54,6 +106,7 @@ for await (const jsonPath of globber.globGenerator()) {
 		core.info(`${styles.green.open}✔ file ${jsonPathRelative} is valid${styles.green.close}`);
 	} else {
 		core.info(`${styles.red.open}✖︎ file ${jsonPathRelative} is invalid${styles.red.close}`);
+		errorOutput = errorOutput + `File ${jsonPathRelative} is invalid\n` + JSON.stringify(validateFile.errors, null, 2) + `\n\n`;
 		errorsCounter++;
 	}
 
@@ -63,9 +116,26 @@ for await (const jsonPath of globber.globGenerator()) {
 		core.info(JSON.stringify(validateFile.errors, null, 2));
 		core.endGroup();
 	}
+
+	// Show Results in summary, if enabled.
+	testCounter++;
 }
 
 // Fail the task run in case of any error
 if (errorsCounter) {
 	core.setFailed(`There are ${errorsCounter} invalid files`);
+} else {
+	errorOutput = errorOutput + `All files passed validation`
+}
+
+if (core.getBooleanInput('show_result_in_summary')) {
+	var passedResults = testCounter - errorsCounter;
+	core.summary.addHeading('Validation Results', 3)
+	.addCodeBlock(`${errorOutput}`, "js")
+	.addTable([
+		[{data: 'Number of files tested', header: true}, `${testCounter}`],
+		[{data: 'Number of files passed', header: true}, `${passedResults}`],
+		[{data: 'Number of files failed', header: true}, `${errorsCounter}`]
+	])
+	.write();		
 }
